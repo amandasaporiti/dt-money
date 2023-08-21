8e393e0afc1301ddc0fe5a1f67455a9084546ceb
amandasaporiti <amandasaporiti@gmail.com>
2023-08-21 17:08:15 -0300

    Update json server data and api base url to production and local

--- src/server/api.ts
+++ src/server/api.ts
@@ -1,5 +1,11 @@
 import axios from 'axios'
 
+// Rodando json server deploy vercel
 export const api = axios.create({
-  baseURL: 'https://dt-money-api-zeta.vercel.app',
+  baseURL: 'https://dt-money-ntzvn6awp-amandasaporiti-gmailcom.vercel.app/',
+})
+
+// Rodando json server localmente
+export const apiLocal = axios.create({
+  baseURL: ' http://localhost:3000/',
 })
